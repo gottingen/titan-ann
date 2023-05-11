c7351d74d8a2ad0aa6bd4cd1570a0cf808193405
bohuli2048 <lijippy@163.com>
2023-05-12 03:13:21 +0800

    add fast flags

--- CMake/deps.cmake
+++ CMake/deps.cmake
@@ -2,15 +2,6 @@
 include(CPM_v0380)
 
 CPMAddPackage(
-    NAME openmp
-    GITHUB_REPOSITORY "gottingen/openmp"
-    GIT_TAG 11daa2021c590dc74a0e734b4783570b619d88c9
-    EXCLUDE_FROM_ALL YES
-    OPTIONS "CMAKE_BUILD_TYPE Release"
-            "OPENMP_ENABLE_LIBOMPTARGET OFF"
-            "OPENMP_STANDALONE_BUILD ON")
-
-CPMAddPackage(
         NAME turbo
         GITHUB_REPOSITORY "gottingen/turbo"
         GIT_TAG v0.8.6
