5e7513c73ca48084666981eec10c188b80cd5b0e
bohuli2048 <lijippy@163.com>
2023-05-24 13:56:40 +0800

    fix conda deps

--- conda/bld-py.sh
+++ conda/bld-py.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-cmake -B buildpy_${PY_VER} -DPython_EXECUTABLE=$PYTHON python
+cmake -B buildpy_${PY_VER} -DCMAKE_BUILD_TYPE=Release \
+                           -DCARBIN_BUILD_TEST=OFF \
+                           -DCARBIN_BUILD_BENCHMARK=OFF \
+                           -DCARBIN_BUILD_EXAMPLES=OFF \
+                           -DCARBIN_USE_CXX11_ABI=ON \
+                           -DBUILD_SHARED_LIBRARY=ON \
+                           -DCMAKE_INSTALL_LIBDIR=lib \
+                           -DBUILD_STATIC_LIBRARY=OFF \
+                           -DPython_EXECUTABLE=$PYTHON python
 
 cmake --build buildpy_${PY_VER}
 cp -r python/tannpy buildpy_${PY_VER}/python
